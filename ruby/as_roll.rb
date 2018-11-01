bca1994e8aa2792354e6c4a9e6297d9399e46ab9
jtb <josephtylerbrowning@gmail.com>
2018-11-01 13:17:19 -0400

    Update as_roll.rb

--- ruby/as_roll.rb
+++ ruby/as_roll.rb
@@ -1,11 +1,16 @@
-sum = 0
-6.times do
-  rolls = []
-  4.times do
-    rolls << (rand * 6).ceil
+#!/usr/local/bin/ruby
+
+stats = []
+loop do
+  6.times do
+    rolls = []
+    4.times do
+      rolls << rand(1..6)
+    end
+    stats << rolls.sort[1..-1].sum
   end
-  rolls.delete rolls.min
-  sum += rolls.sum
-  puts rolls.sum
+  break if stats.min < 8 && stats.max > 16
+  stats = []
 end
-puts "sum: #{sum}"
+
+puts stats
